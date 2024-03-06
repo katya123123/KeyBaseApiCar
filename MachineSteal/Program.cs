6c6bb491903418e7f6141d7abfa5b8884492b3aa
calink <caplink777@gmail.com>
2024-03-06 23:18:25 +1000

    fix

--- MachineSteal/Program.cs
+++ MachineSteal/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "FarPostTeam present\nВорМашин");
+app.MapGet("/", () => "FarPostTeam present\nKBAC");
 
 app.Run();
